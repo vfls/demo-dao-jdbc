3532e6a2ffb1c7e10a40631788b9303e89de0411
vfls <vitorfelipe.s@hotmail.com>
2023-12-03 17:16:54 -0300

    Department entity class

---
+++ src/model/entities/Department.java
@@ -0,0 +1,54 @@
+package model.entities;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class Department implements Serializable {
+    @Serial
+    private static final long serialVersionUID =1L;
+    private Integer id;
+    private String name;
+
+    public Department (Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Department that)) return false;
+
+        return getId() != null ? getId().equals(that.getId()) : that.getId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() != null ? getId().hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
+
