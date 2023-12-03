29c660fe624e6a86fda6c6386c074ca9700f8c8f
vfls <vitorfelipe.s@hotmail.com>
2023-12-03 18:40:36 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,9 +1,11 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
+import model.entities.Department;
 
 public class DaoFactory {
     public static SellerDao createSellerDao(){
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection());
     }
 }
